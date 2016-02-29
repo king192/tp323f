bf8c531af6aaf8c2494bdcc7f340c327f5ce9984
xhq <1434970057@qq.com>
2016-02-29 18:14:21 +0800

    kdf

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -5,4 +5,13 @@ class IndexController extends Controller {
     public function index(){
         $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
     }
+    public function location(){
+    	var_dump($_SERVER);
+		$ip = get_client_ip();
+		$Ip = new \Org\Net\IpLocation('UTFWry.dat'); // 实例化类 参数表示IP地址库文件
+		$area = $Ip->getlocation('203.34.5.66'); // 获取某个IP地址所在的位置
+		echo $ip;
+		var_dump($area);
+		var_dump($IP);
+    }
 }
